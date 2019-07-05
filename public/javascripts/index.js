2e3775540b05ab0aad085aae59210e0f16a1aa28
nodir-sulaymonov <s.nodir9494@gmail.com>
2019-07-05 15:06:45 +0500

    push project on git hub

---
+++ public/javascripts/index.js
@@ -0,0 +1,15 @@
+function loveTriangle(array = []) {
+    let count =0;
+    for (let i = 0; i < array.length; i++) {
+        let a = array[i];
+        let b = array[a-1];
+        let c = array[b-1];
+        if(c === i+1){
+            count++;
+        }
+    }
+
+    return count/3;
+}
+
+console.log(loveTriangle([2, 3, 1, 5, 6, 4, 8, 9, 7, 11, 12, 10, 13, 9, 13, 2, 15, 13, 12, 11]));
